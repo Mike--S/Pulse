f219635299ff01b8a738e39385025d796aa51fac
Michael Shakmakov <Michael_Shakmakov@epam.com>
2016-07-10 02:07:00 +0400

    counter functionality removing

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from '../containers/App';
-import CounterPage from '../containers/CounterPage';
 import AnotherPage from '../containers/AnotherPage';
 import NotFoundPage from '../containers/NotFoundPage';
 
 export default (
   <Route component={App}>
-    <Route path="/" component={CounterPage} />
+    <Route path="/" component={AnotherPage} />
     <Route path="/another" component={AnotherPage} />
     <Route path="*" component={NotFoundPage} />
   </Route>
