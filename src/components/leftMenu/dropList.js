32d156226c810c1a435cca95fb6c26c3f1a343fb
Michael Shakmakov <Michael_Shakmakov@epam.com>
2016-07-13 01:45:44 +0400

    toggle menu

--- src/components/leftMenu/dropList.js
+++ src/components/leftMenu/dropList.js
@@ -2,13 +2,16 @@ import React, { Component, PropTypes } from 'react';
 import cssModules from 'react-css-modules';
 import dl from './dropList.scss';
 import utils from '../../assets/utils.scss';
-import Button from '../../components/button/button';
+import leftMenu from '../../containers/LeftMenu/leftMenu.scss'
 
-@cssModules([dl, utils])
+import Button from '../../components/button/button';
+import Icons from '../materialUi/icons/icons';
+
+@cssModules([dl, utils, leftMenu])
 
 export default class dropList extends Component {
   static propTypes = {
-    deviceList: PropTypes.array,
+    deviceList: PropTypes.array
   };
 
   constructor() {
@@ -21,13 +24,12 @@ export default class dropList extends Component {
     this.toggleMenu = this.toggleMenu.bind(this);
   }
 
-  toggleMenu(event) {
+  toggleMenu(lm, event) {
     this.setState({opened: !this.state.opened});
     var element = event.target;
-    var body = document.querySelector('body');
 
     setTimeout(()=> {
-      document.querySelectorAll('nav')[1].scrollTop += element.nextSibling.offsetHeight;
+      document.querySelector('.' + lm.leftMenu).scrollTop += element.nextSibling ? element.nextSibling.offsetHeight : 0;
     });
   }
 
@@ -35,18 +37,23 @@ export default class dropList extends Component {
     const { styles, title, devices, buttonText } = this.props;
     const dl = styles[0];
     const u = styles[1];
+    const lm = styles[2];
+
     let deviceList = devices ? devices.map((device)=>{
       return (
-        <li className={dl.device}>
+        <li className={dl.device}>icons
           <span>{device.name}</span>
-          <span>{device.id}</span>
+          <span className={u.right}>{device.id}</span>
         </li>
       );
     }) : '';
 
     return (
       <div>
-        <div className={dl.title} onClick={this.toggleMenu}>{title}</div>
+        <div className={dl.title} onClick={this.toggleMenu.bind(this, lm)}>
+          {title}
+          <span className={dl.icon}><Icons iconName={this.state.opened ? "expand" : "collapse"} /></span>
+        </div>
         <section style={ {'display' : this.state.opened ? 'block':'none' } }>
           <ul className={u.clearSpaces + ' ' + dl.list}>
             {deviceList}
